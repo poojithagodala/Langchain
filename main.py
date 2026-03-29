081a915c2f67ddac27dee8a7ece230576c87d7da
Poojitha Godala <poojithagodala99@gmail.com>
2026-03-29 17:43:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 from dotenv import load_dotenv
-from langchain_ollama import ChatOllama
-from langchain_core.prompts import PromptTemplate
-import os
 
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import OpenAI, ChatOpenAI
+
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+       query: The query to search for
+    Returns:
+       The search result
+
+    """
+    print("Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
 
 def main():
-    print("Hello from langchain-course!")
+    print("Hello from langchain")
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo")})
 
-    info = """
-Elon Reeve Musk is a businessman and entrepreneur known 
-for Tesla, SpaceX, X, and xAI.
-    """
-
-    summary_template = """
-Given the info: {info} about a person, create:
-1. A short summary
-2. Two interesting facts about them
-    """
-
-    prompt = PromptTemplate.from_template(summary_template)
-
-    llm = ChatOllama(temperature=0, model="gemma3:270m")
-    ##llm = ChatOllama(temperature=0, model="gpt-5")
-    chain = prompt | llm
-
-    response = chain.invoke({"info": info})
-
-    print(response.content)
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+     main()
